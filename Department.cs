240b30fa114d983807224c44b5853c42304b627d
Mrunali Walke <walkemrunali11@gmail.com>
2022-03-24 22:20:26 +0530

    Add project files.

---
+++ Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable] // Attribute-->Informs at runtime that this class allow to serialize
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace Serialization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSoapWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Department dept = new Department();
+                dept.Id = Convert.ToInt32(txtId.Text);
+                dept.Name = txtName.Text;
+                dept.Location = txtLocation.Text;
+                FileStream fs = new FileStream(@"D:\DeptSoap.soap", FileMode.Create, FileAccess.Write);
+                SoapFormatter sf = new SoapFormatter();
+                sf.Serialize(fs, dept);
+                fs.Close();
+                MessageBox.Show("file added");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void departID_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Department dept = new Department();
+                dept.Id = Convert.ToInt32(txtId.Text);
+                dept.Name = txtName.Text;
+                dept.Location = txtLocation.Text;
+                FileStream fs = new FileStream(@"D:\DeptBinary.dat", FileMode.Create, FileAccess.Write);
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, dept);
+                fs.Close();
+                MessageBox.Show("file added");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnJsonWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //1 . store data into the object
+                Department dept = new Department();
+                dept.Id = Convert.ToInt32(txtId.Text);
+                dept.Name = txtName.Text;
+                dept.Location = txtLocation.Text;
+                //2. Create a file & open in write mode 
+                FileStream fs = new FileStream(@"D:\Dept.json", FileMode.Create, FileAccess.Write);
+                //3. use serliaze method 
+                JsonSerializer.Serialize(fs, dept);
+                fs.Close();
+                MessageBox.Show("File Added");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnJsonRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Department dept = new Department();
+                FileStream fs = new FileStream(@"D:\Dept.json", FileMode.Open, FileAccess.Read);
+                dept = JsonSerializer.Deserialize<Department>(fs);
+                txtId.Text = dept.Id.ToString();
+                txtName.Text = dept.Name;
+                txtLocation.Text = dept.Location;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnXmlWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //1 . store data into the object
+                Department dept = new Department();
+                dept.Id = Convert.ToInt32(txtId.Text);
+                dept.Name = txtName.Text;
+                dept.Location = txtLocation.Text;
+                //2. Create a file & open in write mode 
+                FileStream fs = new FileStream(@"D:\DeptXmlFile.xml", FileMode.Create, FileAccess.Write);
+                //3. use serliaze method 
+                XmlSerializer xml = new XmlSerializer(typeof(Department));
+                xml.Serialize(fs, dept);
+                fs.Close();
+                MessageBox.Show("File Added");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnXmlRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Department dept = new Department();
+                FileStream fs = new FileStream(@"D:\DeptXmlFile.xml", FileMode.Open, FileAccess.Read);
+                XmlSerializer xml = new XmlSerializer(typeof(Department));
+                dept = (Department)xml.Deserialize(fs);
+                txtId.Text = dept.Id.ToString();
+                txtName.Text = dept.Name;
+                txtLocation.Text = dept.Location;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnBinaryWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Department dept = new Department();
+                dept.Id = Convert.ToInt32(txtId.Text);
+                dept.Name = txtName.Text;
+                dept.Location = txtLocation.Text;
+                FileStream fs = new FileStream(@"D:\DeptBinary.dat", FileMode.Create, FileAccess.Write);
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, dept);
+                fs.Close();
+                MessageBox.Show("file added");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnBinaryRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Department dept = new Department();
+                FileStream fs = new FileStream(@"D:\DeptBinary.dat", FileMode.Open, FileAccess.Read);
+                BinaryFormatter bf = new BinaryFormatter();
+                dept = (Department)bf.Deserialize(fs);
+                txtId.Text = dept.Id.ToString();
+                txtName.Text = dept.Name;
+                txtLocation.Text = dept.Location;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSoapRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Department dept = new Department();
+                FileStream fs = new FileStream(@"D:\DeptSoap.soap", FileMode.Open, FileAccess.Read);
+                SoapFormatter sf = new SoapFormatter();
+                dept = (Department)sf.Deserialize(fs);
+                txtId.Text = dept.Id.ToString();
+                txtName.Text = dept.Name;
+                txtLocation.Text = dept.Location;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
